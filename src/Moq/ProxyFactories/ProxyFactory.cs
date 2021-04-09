3760a1213f439b8ce1a17c6968a1d9e73be3bd20
bclothier <bgclothier@gmail.com>
2021-04-08 22:53:22 -0500

    This is a ugly, unofficial, unsupported hack. Please don't use it. Really. Just don't.
    This is to work around a very very specific problem of proxy generator caching types that has been changed at runtime and allowing invalidation of the cached type as to avoid problems with using stale version of the dynamically created types.
    
    You shouldn't do it.

--- src/Moq/ProxyFactories/ProxyFactory.cs
+++ src/Moq/ProxyFactories/ProxyFactory.cs
@@ -6,12 +6,28 @@ using System.Reflection;
 
 namespace Moq
 {
+	/// <summary>
+	/// Horrible, horrible, ugly, terrible, no-good hack! Go away! You did not see this!
+	/// </summary>
+	public static class ProxyCache
+	{
+		/// <summary>
+		/// Dude, why are you even here? You are not supposed to be looking at this. Get lost. Use nuget!
+		/// </summary>
+		public static void Invalidate() => ProxyFactory.InvalidateInstance();
+	}
+
 	internal abstract class ProxyFactory
 	{
 		/// <summary>
 		/// Gets the global <see cref="ProxyFactory"/> instance used by Moq.
 		/// </summary>
-		public static ProxyFactory Instance { get; } = new CastleProxyFactory();
+		public static ProxyFactory Instance { get; private set; } = new CastleProxyFactory();
+		/// <summary>
+		/// This is the unofficial hack. This is not approved. Use it at your own risk and if it breaks,
+		/// congratulations you have bought the farm!
+		/// </summary>
+		internal static void InvalidateInstance() => Instance = new CastleProxyFactory();
 
 		public abstract object CreateProxy(Type mockType, IInterceptor interceptor, Type[] interfaces, object[] arguments);
 
